3369aba34f5baf6252fd065193fbbe66aed7b38b
marcprz465 <mprzyb@outlook.com>
2019-03-19 12:03:57 +0100

    Added filereader

---
+++ src/Reader.java
@@ -0,0 +1,50 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class Reader {
+	private ArrayList<String> teams;
+	public Reader() throws IOException
+	{
+		teams=new ArrayList<String>();
+	}
+public void read() throws IOException  
+{
+	//String file_location=System.getProperty("user.dir")+"/teams.txt";
+	
+	FileReader file=new FileReader(System.getProperty("user.dir")+"//src//teams.txt");
+	//FileReader file=new FileReader(file_location);
+	if(file.equals(null))
+		System.out.println("file error");
+BufferedReader reader=new BufferedReader(file);
+if(reader.equals(null))
+	System.out.println("reader error");
+String temp="";
+while((temp=reader.readLine())!=null)
+{
+	System.out.println(temp);
+teams.add(temp);
+
+}
+reader.close();
+
+	System.out.println(teams.size());
+
+System.out.println("successed");
+}
+
+
+public ArrayList<String> getTeams() {
+	return teams;
+}
+public void setTeams(ArrayList<String> teams) {
+	this.teams = teams;
+}
+public static void main(String[] args) throws IOException  
+{
+	Reader test=new Reader();
+	test.read();
+}
+}
